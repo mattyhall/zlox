25f8a3f3506ef09eaf09e56763dceb0ca575c5bd
Matthew Hall <matthew@quickbeam.me.uk>
2021-10-04 22:38:38 +0100

    Add constants and const opcode

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,32 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
-const OpCode = enum {
+const OpCode = enum(u8) {
     ret,
+    constant,
 };
+
+const Value = f32;
 
 const Chunk = struct {
     code: [*]u8,
     count: usize,
     capacity: usize,
+
+    values: std.ArrayList(Value),
+
     allocator: *Allocator,
 
     const Self = @This();
 
     fn init(allocator: *Allocator) Self {
-        return Self{ .allocator = allocator, .code = @intToPtr([*]u8, 0xdeadbeef), .count = 0, .capacity = 0 };
+        return Self{
+            .allocator = allocator,
+            .code = @intToPtr([*]u8, 0xdeadbeef),
+            .count = 0,
+            .capacity = 0,
+            .values = std.ArrayList(Value).init(allocator),
+        };
     }
 
     fn codeSlice(self: *Self) ?[]u8 {
@@ -26,9 +38,18 @@ const Chunk = struct {
         return res;
     }
 
+    fn addConstant(self: *Self, val: Value) !u8 {
+      if (self.values.items.len == 256)
+        return error.TooManyConstants;
+
+      try self.values.append(val);
+      return @intCast(u8, self.values.items.len - 1);
+    }
+
     fn writeChunk(self: *Self, byte: u8) !void {
         if (self.capacity < self.count + 1) {
             if (self.capacity == 0) {
+                self.capacity = 8;
                 self.code = (try self.allocator.alloc(u8, 8)).ptr;
             } else {
                 self.capacity *= 2;
@@ -43,8 +64,14 @@ const Chunk = struct {
         try stdout.print("{x:0>4} ", .{offset});
         switch (@intToEnum(OpCode, self.code[offset])) {
             .ret => {
-                try stdout.print(" OP_RETURN\n", .{});
+                try stdout.print(" {s:<5}\n", .{"RET"});
                 return offset + 1;
+            },
+            .constant => {
+              const index = self.code[offset + 1];
+              const val = self.values.items[index];
+              try stdout.print(" {s:<5} {} ({})\n", .{"CONST", index, val});
+              return offset + 2;
             },
         }
     }
@@ -60,6 +87,7 @@ const Chunk = struct {
 
     fn deinit(self: *Self) void {
         self.allocator.deinit(self.code);
+        self.values.deinit();
     }
 };
 
@@ -67,7 +95,12 @@ pub fn main() anyerror!void {
     std.log.info("All your codebase are belong to us.", .{});
     var data: [16 * 1024]u8 = undefined;
     var alloc = std.heap.FixedBufferAllocator.init(&data);
+
     var chunk = Chunk.init(&alloc.allocator);
+    const constant = try chunk.addConstant(1.3);
+    try chunk.writeChunk(@enumToInt(OpCode.constant));
+    try chunk.writeChunk(constant);
     try chunk.writeChunk(@enumToInt(OpCode.ret));
+
     try chunk.disassemble("test chunk");
 }
